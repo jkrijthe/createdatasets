8699bbbbf9969cc6595b0b983ba29eb4fcede085
Jesse Krijthe <jkrijthe@gmail.com>
2014-12-03 13:56:33 +0100

    Documentation update

--- R/createdatasets.R
+++ R/createdatasets.R
@@ -3,5 +3,5 @@
 #' This package allows one to download (if neccesary) and preprocess benchmark datasets from the UCI machine learning repository and other sources for the use in evaluating classification and regression methods.These functions can be helpful in setting up simulation studies that can be easily reproduced by others.
 #' 
 #' @docType package
-#' @name RSSL
+#' @name createdatasets
 NULL
